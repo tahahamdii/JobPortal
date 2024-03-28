115d568efd5a9f5dd40e54d8fccb591a6fe4cecf
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-03-28 05:14:24 +0100

    auth page working

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { Office } from "../assets";
+import { SignUp } from "../components";
 
 const Auth = () => {
   const  {user} = useSelector((state) => state.user) 
-  const [open , setOpen] = React.useState(false);
+  const [open , setOpen] = useState(true);
   const location = useLocation();
 
-  let from = location.state?.from || { pathname: "/" };
+  let from = location.state?.from?.pathname || "/" ;
   if(user.token) {
     return window.location.replace(from);
   
   }
   return (
-    <div>Auth</div>
+    <div className='w-full '>
+      <img src={Office} alt='Office' className='object-contain ' />
+
+      <SignUp open={open} setOpen={setOpen} />
+    </div>
   )
 }
 
