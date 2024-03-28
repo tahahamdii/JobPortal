38d7a7bc75ca142146707174d7a4474a55685ca2
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-03-28 04:54:23 +0100

    start auth

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Auth = () => {
+  const  {user} = useSelector((state) => state.user) 
+  const [open , setOpen] = React.useState(false);
+  const location = useLocation();
+
+  let from = location.state?.from || { pathname: "/" };
+  if(user.token) {
+    return window.location.replace(from);
+  
+  }
   return (
     <div>Auth</div>
   )
