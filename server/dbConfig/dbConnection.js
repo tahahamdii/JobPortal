7ab3ebfe42e9ab776df7987937c836bd8a69cc79
Med Taha Hamdi <116748061+tahahamdii@users.noreply.github.com>
2024-04-11 01:34:36 +0200

    Update dbConnection.js

--- server/dbConfig/dbConnection.js
+++ server/dbConfig/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Connect to the database
  const  dbConnection = async () => {
     try {
-        await mongoose.connect('mongodb+srv://taha:taha123@cluster0.p5yoqci.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('');
         console.log("Connected to the database");
     } catch (error) {
         console.log("Error connecting to the database", error);
