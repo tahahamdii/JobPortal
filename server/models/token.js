edbbfb912b2e86e6c3a3b9680309be51bdef2924
Hamdi Taha <hamdi.taha@esprit.tn>
2024-04-10 23:30:45 +0200

    ref

---
+++ server/models/token.js
@@ -0,0 +1,11 @@
+const mongoose  = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tokenSchema = new Schema({
+    userId: {
+        type:Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+        unique: true,
+    }
+})
